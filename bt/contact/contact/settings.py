c3f28f125dc18f358d9ad760c841e3d5752b68b0
berginaluidgy <156596145+berginaluidgy@users.noreply.github.com>
2024-07-30 23:48:43 -0400

    Update settings.py
    1

--- bt/contact/contact/settings.py
+++ bt/contact/contact/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mdkf*gp$pe4%cov7jczelcxxz=&l_c#=wh!w6s9j$_$bn*i%(p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
